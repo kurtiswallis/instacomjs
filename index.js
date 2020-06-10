9d983e9bd3aa166bb56c44c77a6d4e42b09704b7
kurtiswallis <57373636+kurtiswallis@users.noreply.github.com>
2020-06-10 14:24:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var Comm100API=Comm100API||{};(function(t){function e(e){var a=document.createElement("script"),c=document.getElementsByTagName("script")[0];a.type="text/javascript",a.async=!0,a.src=e+t.site_id,c.parentNode.insertBefore(a,c)}t.chat_buttons=t.chat_buttons||[],t.chat_buttons.push({code_plan:128,div_id:"livechat-button-128"}),t.site_id=6000088,t.main_code_plan=128,e("https://instacom.io/vue/livechat.ashx?siteId="),setTimeout(function(){t.loaded||e("https://instacom.io/vue/livechat.ashx?siteId=")},5e3)})(Comm100API||{})
 
+document.getElementById("livechat-button").click();
